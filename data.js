d630f0d27aab6ec82dfe7e5ab545d94788e9a3d8
ddynikov <ddynikov@gmail.com>
2021-06-11 18:11:48 +0300

    initial commit

---
+++ data.js
@@ -0,0 +1,27 @@
+const pass = {
+	string: 'password1234',
+	smthStrange: 'takeMeToTheMo00N@!',
+	numPass: 14092001,
+	grandFather: 'GrandSonBirthday14092001',
+	regExp: /d(b+)d/g,
+	emoji: '😀 😃 😄 😁',
+	ukr: 'Слава Україні!',
+	specSymb: '^^$#@!:',
+};
+
+const input = {
+	num: 1409,
+	string: 'teststring',
+	cyr: 'Словяне вперёд!',
+	specSymb: '^^$#@!:',
+	arr: [1, {key: 19}],
+	obj: {key: 'value', num: 19},
+	emoji: '😀 😃 😄 😁',
+	arabic: 'الله أكبر', 
+	japan: 'こんにちは', 
+};
+
+module.exports = {
+	pass,
+	input,
+}
---
+++ lab2.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { scrypt } = require('crypto');
+
+// const hashFunc = scrypt('password', 'salt', 64, (err, derivedKey) => {
+// 	if (err) throw err;
+// 	console.log(derivedKey.toString('hex'));  // '3745e48...08d59ae'
+// });
+
+// const hashFunc = ('pass', 'salt', 64, (err, derivedKey) => {
+// 	if (err) console.error(err);
+// 	console.log(derivedKey.toString('hex'));  // '3745e48...08d59ae'
+// 	return scrypt(pass, salt, 64)
+// });
+
+const hashFunc = (hashData) => {
+	const { salt, password } = hashData;
+	const keylen = hashData.keylen ? hashData.keylen : defaultKeyLen;
+  
+	return scrypt(password, salt, keylen, (err, derivedKey) => {
+		if (err) throw err;
+		console.log(derivedKey);  // '3745e48...08d59ae'
+	});
+  };
+
+module.exports = {
+	hashFunc
+}
