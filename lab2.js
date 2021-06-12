c0f7f6f2e6aabd2c342072b89ad1fd37769dac75
ddynikov <ddynikov@gmail.com>
2021-06-12 20:19:54 +0300

    comments deleted & bcrypt removed

--- lab2.js
+++ lab2.js
@@ -1,18 +1,6 @@
 'use strict';
 
 const { scryptSync } = require('crypto');
-const bcrypt = require('bcrypt');
-
-// const hashFunc = scrypt('password', 'salt', 64, (err, derivedKey) => {
-// 	if (err) throw err;
-// 	console.log(derivedKey.toString('hex'));  // '3745e48...08d59ae'
-// });
-
-// const hashFunc = ('pass', 'salt', 64, (err, derivedKey) => {
-// 	if (err) console.error(err);
-// 	console.log(derivedKey.toString('hex'));  // '3745e48...08d59ae'
-// 	return scrypt(pass, salt, 64)
-// });
 
 const hashFunc = (hashData) => {
 	const { salt, password } = hashData;
