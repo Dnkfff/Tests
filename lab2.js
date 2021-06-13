0c17b61692693f44fe979e5717a457eaeca1eaf0
smithd-roses <smithd.contact@gmail.com>
2021-06-13 13:25:32 +0300

    run prettier

--- lab2.js
+++ lab2.js
@@ -1,15 +1,16 @@
-const { scrypt } = require('crypto');
+const { scrypt } = require('crypto')
 
 // promisify scrypt with callback
-const hashFunc = ({ salt, password, keylen = 64 }) => (new Promise(resolve => {
-  scrypt(password, salt, keylen, (err, derivedKey) => {
-    if (err) {
-      throw err;
-    }
-    resolve(derivedKey);
-  });
-}))
+const hashFunc = ({ salt, password, keylen = 64 }) =>
+  new Promise((resolve) => {
+    scrypt(password, salt, keylen, (err, derivedKey) => {
+      if (err) {
+        throw err
+      }
+      resolve(derivedKey)
+    })
+  })
 
 module.exports = {
-	hashFunc
-};
+  hashFunc,
+}
