9b99d87860f156b8511c6234bb00c42aa4f3b136
Danil <ddynikov@gmail.com>
2022-10-07 13:41:45 +0300

    Update validTestData.js

--- testdata/validTestData.js
+++ testdata/validTestData.js
@@ -2,21 +2,23 @@ const pass = {
 	string: 'password1234',
 	smthStrange: 'takeMeToTheMo00N@!',
 	num: 14092001,
-	grandFather: 'GrandSonBirthday14092001',
+	grandFather: 'GrandSonBirthday--/--/--',
 	regExp: /d(b+)d/g,
 	emoji: '😀 😃 😄 😁',
 	ukr: 'Слава Україні!',
+	pass: 'Паляниця',
 	specSymb: '^^$#@!:',
 };
 
 const input = {
-	num: 1409,
-	string: 'teststring',
-	cyr: 'Словяне вперёд!',
+	num: 1415926,
+	string: 'testString',
+	kac: 'Словяне вперёд!',
 	specSymb: '^^$#@!:',
 	emoji: '😀 😃 😄 😁',
 	arabic: 'الله أكبر', 
-	japan: 'こんにちは', 
+	japanese: 'こんにちは',
+	chineese: '榮耀歸於烏克蘭',
 };
 
 module.exports = {
